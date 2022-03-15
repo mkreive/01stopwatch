d562c05f608271c3070a85f33c049b0cef1e8022
mkreive <mkreive@gmail.com>
2022-03-15 14:40:43 +0200

    document selectors

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log("hello");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const milisec = document.getElementById("milisec");
+
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+start.addEventListener("click", function (e) {
+    console.log("clicked start");
+});
+
+stop.addEventListener("click", function (e) {
+    console.log("clicked");
+});
+
+reset.addEventListener("click", function (e) {
+    console.log("clicked");
+});
