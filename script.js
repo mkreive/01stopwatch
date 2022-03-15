dfb3b179487c6216e82517a11dc2ab44d9b7ab11
mkreive <mkreive@gmail.com>
2022-03-15 16:08:39 +0200

    seconds timer

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
-"use strict";
+// "use strict";
 
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
-const milisec = document.getElementById("milisec");
+const showMinutes = document.getElementById("minutes");
+const showSeconds = document.getElementById("seconds");
+const showMiliseconds = document.getElementById("milisec");
 
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-const reset = document.getElementById("reset");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
 
-start.addEventListener("click", function (e) {
-    console.log("clicked start");
+let min = 00;
+let sec = 00;
+let milsec = 00;
+
+let timeCounter;
+
+startBtn.addEventListener("click", function () {
+    timeCounter = setInterval(() => {
+        showMinutes.textContent = min.toLocaleString("en-US", {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+        });
+        showSeconds.textContent = sec.toLocaleString("en-US", {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+        });
+        showMiliseconds.textContent = milsec.toLocaleString("en-US", {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+        });
+        sec++;
+    }, 1000);
 });
 
-stop.addEventListener("click", function (e) {
-    console.log("clicked");
+stopBtn.addEventListener("click", function () {
+    clearInterval(timeCounter);
+    timeCounter = null;
+    startBtn.textContent = "resume";
 });
 
-reset.addEventListener("click", function (e) {
-    console.log("clicked");
+resetBtn.addEventListener("click", function () {
+    min = 00;
+    sec = 00;
+    milsec = 00;
+    showMinutes.textContent = min.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+    });
+    showSeconds.textContent = sec.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+    });
+    showMiliseconds.textContent = milsec.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+    });
+    console.log(sec);
 });
+
+const startTimer = function () {};
